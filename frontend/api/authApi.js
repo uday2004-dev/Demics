ceee7e1387761da96871f24c9159f270a26dbeb2
Uday Shrivastav <udayshrivastav2004@gmail.com>
2026-07-10 19:08:42 +0530

    correct authapi file

--- frontend/api/authApi.js
+++ frontend/api/authApi.js
@@ -1,12 +1,22 @@
-import axios from "axios";
+// import axios from "axios";
+// import api from "./axios";
+
+// export const loginAdmin=(data)=>{
+//     return api.post("/api/admin/adminLogin",data)
+
+// }
+
+
+// export const logoutAdmin = () => {
+//   return api.post("/api/admin/logout");
+// };  
+
 import api from "./axios";
 
-export const loginAdmin=(data)=>{
-    return api.post("/admin/adminLogin",data)
-
-}
-
+export const loginAdmin = (data) => {
+  return api.post("/api/admin/adminLogin", data);
+};
 
 export const logoutAdmin = () => {
-  return api.post("/admin/logout");
-};  
+  return api.post("/api/admin/adminLogout");
+};
