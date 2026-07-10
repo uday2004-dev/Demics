135d8480642f22f633daee60666233fcd536083d
Uday Shrivastav <udayshrivastav2004@gmail.com>
2026-07-10 23:33:33 +0530

    add some code in package.json

--- backend/middleware/adminAuthMiddleware.js
+++ backend/middleware/adminAuthMiddleware.js
@@ -1,87 +1,87 @@
-// import cookies from "cookie-parser"
-// import jwt from "jsonwebtoken"
+import cookies from "cookie-parser"
+import jwt from "jsonwebtoken"
 
-// export const adminAuthMiddleware = (req, res, next) => {
+export const adminAuthMiddleware = (req, res, next) => {
 
      
-//     try {
-//           console.log(req.cookies); 
-//     const token = req.cookies.token
-//     if(!token){
-//        return res.status(401).json({
-//             success:false,
-//             message:"admin is not authorized"
-//         })
-//     }
-
-//     const decode=jwt.verify(
-//         token,
-//         process.env.JWT_SECRET
-//     )
-
-
-//     if(decode.type!=="admin"){
-//         return res.status(403).json({
-//             success:false,
-//             message:"unauthorized"
-//         })
-//     }
-
-//     req.admin=decode
-//     next()
-
-
-//     } catch (error) {
-//         console.log(error)
-//         return res.status(401).json(
-//             {
-//                 success: false,
-//                 message: "user is not authenticated"
-//             }
-//         )
-//     }
-// }
-
-// export default adminAuthMiddleware
-
-
-import jwt from "jsonwebtoken";
-
-const adminAuthMiddleware = (req, res, next) => {
-  try {
-    console.log("========== ADMIN AUTH ==========");
-    console.log("Cookies:", req.cookies);
-
-    const token = req.cookies.token;
-    console.log("Token:", token);
-
-    if (!token) {
-      return res.status(401).json({
-        success: false,
-        message: "Admin is not authorized",
-      });
+    try {
+          console.log(req.cookies); 
+    const token = req.cookies.token
+    if(!token){
+       return res.status(401).json({
+            success:false,
+            message:"admin is not authorized"
+        })
     }
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log("Decoded:", decoded);
+    const decode=jwt.verify(
+        token,
+        process.env.JWT_SECRET
+    )
 
-    if (decoded.type !== "admin") {
-      return res.status(403).json({
-        success: false,
-        message: "Unauthorized",
-      });
+
+    if(decode.type!=="admin"){
+        return res.status(403).json({
+            success:false,
+            message:"unauthorized"
+        })
     }
 
-    req.admin = decoded;
-    next();
-  } catch (error) {
-    console.log("AUTH ERROR:", error.message);
+    req.admin=decode
+    next()
 
-    return res.status(401).json({
-      success: false,
-      message: "User is not authenticated",
-    });
-  }
-};
 
-export default adminAuthMiddleware;
+    } catch (error) {
+        console.log(error)
+        return res.status(401).json(
+            {
+                success: false,
+                message: "user is not authenticated"
+            }
+        )
+    }
+}
+
+export default adminAuthMiddleware
+
+
+// import jwt from "jsonwebtoken";
+
+// const adminAuthMiddleware = (req, res, next) => {
+//   try {
+//     console.log("========== ADMIN AUTH ==========");
+//     console.log("Cookies:", req.cookies);
+
+//     const token = req.cookies.token;
+//     console.log("Token:", token);
+
+//     if (!token) {
+//       return res.status(401).json({
+//         success: false,
+//         message: "Admin is not authorized",
+//       });
+//     }
+
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     console.log("Decoded:", decoded);
+
+//     if (decoded.type !== "admin") {
+//       return res.status(403).json({
+//         success: false,
+//         message: "Unauthorized",
+//       });
+//     }
+
+//     req.admin = decoded;
+//     next();
+//   } catch (error) {
+//     console.log("AUTH ERROR:", error.message);
+
+//     return res.status(401).json({
+//       success: false,
+//       message: "User is not authenticated",
+//     });
+//   }
+// };
+
+// export default adminAuthMiddleware;
