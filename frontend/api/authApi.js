8ffbbb6071533e4c4a2623425675c9d85a6e3b24
Uday Shrivastav <udayshrivastav2004@gmail.com>
2026-07-10 19:17:25 +0530

    added code

--- frontend/api/authApi.js
+++ frontend/api/authApi.js
@@ -13,18 +13,18 @@
 
 import api from "./axios";
 
-// export const loginAdmin = (data) => {
-//   return api.post("/api/admin/adminLogin", data);
-// };
-
-// export const logoutAdmin = () => {
-//   return api.post("/api/admin/adminLogout");
-// };
-
 export const loginAdmin = (data) => {
-  return api.post("/admin/adminLogin", data);
+  return api.post("/api/admin/adminLogin", data);
 };
 
 export const logoutAdmin = () => {
-  return api.post("/admin/adminLogout");
+  return api.post("/api/admin/adminLogout");
 };
+
+// export const loginAdmin = (data) => {
+//   return api.post("/admin/adminLogin", data);
+// };
+
+// export const logoutAdmin = () => {
+//   return api.post("/admin/adminLogout");
+// };
