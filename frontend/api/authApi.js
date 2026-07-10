003a174898f316b33dc8b72424f3655334f4dcfe
Uday Shrivastav <udayshrivastav2004@gmail.com>
2026-07-10 19:12:51 +0530

    deployment issue

--- frontend/api/authApi.js
+++ frontend/api/authApi.js
@@ -13,10 +13,18 @@
 
 import api from "./axios";
 
+// export const loginAdmin = (data) => {
+//   return api.post("/api/admin/adminLogin", data);
+// };
+
+// export const logoutAdmin = () => {
+//   return api.post("/api/admin/adminLogout");
+// };
+
 export const loginAdmin = (data) => {
-  return api.post("/api/admin/adminLogin", data);
+  return api.post("/admin/adminLogin", data);
 };
 
 export const logoutAdmin = () => {
-  return api.post("/api/admin/adminLogout");
+  return api.post("/admin/adminLogout");
 };
