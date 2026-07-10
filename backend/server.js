b2e10b83dbed0ea5f705fdfeddfbb03e716bb959
Uday Shrivastav <udayshrivastav2004@gmail.com>
2026-07-10 18:58:52 +0530

    update cors part

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://your-frontend-domain.vercel.app",
+      "https://demics-admin.vercel.app",
     ],
     credentials: true,
   })
--- frontend/api/axios.js
+++ frontend/api/axios.js
@@ -1,4 +1,18 @@
+// import axios from "axios";
+// console.log(import.meta.env.VITE_API_URL);
+
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL,
+  
+//   withCredentials: true,
+// });
+
+
+// export default api;
+
 import axios from "axios";
+
+console.log("API URL:", import.meta.env.VITE_API_URL);
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
