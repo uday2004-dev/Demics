1eb73f8d3238733798044a07354bd1d1bc6b96d2
Uday Shrivastav <udayshrivastav2004@gmail.com>
2026-07-10 19:00:45 +0530

    fixed code

--- backend/server.js
+++ backend/server.js
@@ -24,20 +24,30 @@ app.use('/', (req, res) => {
   res.send("Server is running")
 })
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173",
+//       "http://localhost:5174",
+//       "https://demics-admin.vercel.app",
+//     ],
+//     credentials: true,
+//   })
+// );
+
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://demics-admin.vercel.app",
+      "https://frontend-seven-rose-54.vercel.app",
     ],
     credentials: true,
   })
 );
-
 app.use("/api/admin", adminLogin);
 app.use("/api/projects", projectRoutes);
 app.use("/api/services",serviceRoute)
